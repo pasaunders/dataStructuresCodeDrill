b826bd42c047c8bed2f2914f2963d9bd2b8c47df
Willow Saunders <willow.saund@gmail.com>
2026-09-14 13:44:29 -0700

    linked list creation and node creation test

---
+++ src/linked_list.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from collections.abc import Iterable
+from typing import Any
+
+class LinkedList:
+    """ Class representation of a LinkedList"""
+
+    def __init__(self, values : Iterable | None = None) -> None:
+        self.head = None
+        self.length = 0
+        for item in values:
+            self.push(item)
+
+    def push(self, value = None) -> None:
+        self.head = Node(value, self.head)
+        self.length += 1
+
+
+class Node:
+    """Singly linked node containing a value and pointer to another node"""
+    def __init__(self, val: Any | None = None, next: Node | None = None) -> None:
+        self.val = val
+        self.next = next
---
+++ src/test_linked_list.py
@@ -0,0 +1,15 @@
+import pytest
+
+@pytest.fixture
+def sample_linked_list():
+    """Create linked list for testing"""
+    from linked_list import LinkedList
+    one_llist = LinkedList([1])
+    empty_llist = LinkedList()
+    new_llist = LinkedList([1,2,3,4,5])
+
+def test_node_init():
+    """test node class init"""
+    from linked_list import Node
+    new_node = Node(0, None)
+    assert new_node.val == 0 and new_node.next == None
